0d052ea8f9a48bd439e34e7c460b20a21ff54947
Leonardo Bahar <leonardobahar@codedoc.id>
2023-10-03 15:29:59 +0700

    Chore: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "react-bootstrap/Button";
+
+import "./App.css";
+import icon128 from "./assets/images/icon128.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1>Weather App</h1>
+      <img src={icon128} alt="icon128" />
+      <table class="centerContainer">
+        <tr>
+          <td>
+            <label
+              id="cityLabel"
+              for="cityNameTextField"
+              style={{ color: "black" }}
+            >
+              Enter a city:
+            </label>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input id="cityNameTextField" type="text" name="cityName" />
+            <Button id="submitButton" type="submit">
+              Submit
+            </Button>
+          </td>
+        </tr>
+      </table>
+      <div class="spacer"></div>
+      <div
+        id="errorMessageComponent"
+        style={{ display: "none", color: "#fc574e" }}
+      ></div>
+
+      <div
+        id="loader"
+        style={{ display: "none" }}
+        class="spinner spinner-border"
+        role="status"
+      >
+        <span class="sr-only">Loading...</span>
+      </div>
+
+      <div id="weatherInfoComponent" style={{ display: "none" }}>
+        <table class="centerContainer" cellpadding="5px">
+          <tr>
+            <th class="customTableHeader">Temperature</th>
+            <td id="temperatureField">0</td>
+          </tr>
+          <tr>
+            <th class="customTableHeader">Feels Like</th>
+            <td id="feelsLikeField">0</td>
+          </tr>
+          <tr>
+            <th class="customTableHeader">Humidity</th>
+            <td id="humidityField">0</td>
+          </tr>
+          <tr>
+            <th class="customTableHeader">Atmospheric Pressure</th>
+            <td id="pressureField">0</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
